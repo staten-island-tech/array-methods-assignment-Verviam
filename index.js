d8aec9969643a85f24ab5a9daee7b9ce8db9744d
Evan Gao <gaoevan37@gmail.com>
2023-10-10 09:31:17 -0400

    halfway done

---
+++ index.js
@@ -0,0 +1,19 @@
+const students = [
+    {firstName: 'Jason', lastName: 'He', favoriteGame: 'ClashOfClans', student: true, graduated: false, favoriteNumbers: [1,3,4,5,6,1234]}, 
+    {firstName: 'Evan', lastName: 'Cow', favoriteGame: 'n/a', student: true, graduated: false, favoriteNumbers: [2,4,6,8,10]}, 
+    {firstName: 'Evan', lastName: 'Cow', favoriteGame: 'n/a', student: true, graduated: false, favoriteNumbers: [2,4,6,8,10]}, 
+    {firstName: 'Evan', lastName: 'Cow', favoriteGame: 'n/a', student: true, graduated: false, favoriteNumbers: [2,4,6,8,10]}, 
+];
+
+students.forEach((student) => console.log(student.firstName + student.lastName));
+
+students.forEach((student) => console.log(favoriteNumbers)
+
+// change content inside arrays
+
+// write functions to accomplish the following.
+// -Using forEach, print all of the objects names in the console
+// -Using forEach, print all the object’s arrays’ elements in the console one
+// line at a time
+// -Using filter, return a new array by filtering the array based on one of the
+// properties
